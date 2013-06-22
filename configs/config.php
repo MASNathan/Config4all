b89988bfc8698338924410cd0bb13c351ea8a938
ReiDuKuduro <andre.r.flip@gmail.com>
2013-06-22 15:08:15 +0100

    Added extra configs

--- configs/config.php
+++ configs/config.php
@@ -5,3 +5,8 @@ $config['username']	= 'Andre Filipe';
 $config['password']	= 'xpto';
 $config['language']	= 'en';
 $config['serverip']	= '192.168.0.1';
+
+$config['random_stuff']['potatoes'] = 'Potatoes';
+
+$config['random_stuff']['php']['classes'] 	= 'http://www.phpclasses.org/package/8111-PHP-This-class-helps-you-load-your-application-configs.html';
+$config['random_stuff']['php']['github'] 	= 'https://github.com/ReiDuKuduro/Config4all';
