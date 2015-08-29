2d5a0a09d3f725a4cca58af19c781148340d29a2
André Filipe <andre.r.flip@gmail.com>
2015-08-29 13:06:21 +0100

    ...

--- tests/index.php
+++ tests/index.php
@@ -8,7 +8,16 @@ ini_set('display_errors', '1');
 require_once '../vendor/autoload.php';
 
 $config = new MASNathan\Config\Config();
-$config->load('configs/*.yml');
+$config->read('configs/*.xml');
 
-echo '<pre>';
-var_dump($config);
+//$config->write('configs/cache.json');
+
+$config->set('extra.super.extra', 'test');
+$config->set('config.extra', 'test');
+
+//$config->save('asd');
+
+dump($config->get());
+
+
+dump($config);
